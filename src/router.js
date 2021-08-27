52c0cac05f5bc0d21b3e11c82bcfde9cdb37b676
guilherme <guisfanton@hotmail.com>
2021-08-27 10:52:28 -0300

    Primeiro Comit

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+
+  {
+    path: '/forum',
+    name: 'Forum',
+    component: () => import(/* webpackChunkName: "forum" */ '../views/Forum.vue')
+  }
+
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
