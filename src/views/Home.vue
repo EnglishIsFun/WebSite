e73715df12aebeb31f8893b7d781c2d0a8c71bcf
guilherme <guisfanton@hotmail.com>
2021-11-09 17:50:58 -0300

    Home e Jogos parte 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,58 @@
 <template>
   <div class="Home">
-    oioioioioioioio
+    <h1 style="text-align: center">English Is Fun</h1>
+    <br>
+    <v-card class="d-flex">
+      <v-card>
+        <v-card-title>
+          O que é o English Is Fun
+        </v-card-title>
+        <v-card-text>
+          English is Fun é um Projeto de Ensino fornecido pelo Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul - Campus Farroupilha, iniciado no ano de 2020, pela professora Lucilene Bender de Sousa, com o objetivo de fornecer atividades ludicas para ensinar inglês aos alunos do ensino médio de uma forma mais divertida.
+        </v-card-text>
+
+      </v-card>
+      <v-spacer></v-spacer>
+      <v-card>
+        <v-card-title>
+          Resumo do Projeto de Ensino
+        </v-card-title>
+        <v-card-text>
+          A língua inglesa é a língua universal de comunicação e divulgação do conhecimento. Apesar da facilidade de acesso online à língua, seu aprendizado é um processo longo que exige dedicação e persistência, necessitando da mediação de um professor, principalmente nas etapas iniciais de aprendizagem. O ensino de inglês na escola traz inúmeros desafios, dentre eles o número de alunos e a diversidade em termos de níveis de proficiência em uma mesma turma. Assim, tornar o aprendizado significativo e manter a motivação dos estudantes requer muito tempo de planejamento por parte do professor, além de conhecimento e criatividade. O objetivo deste projeto é iniciar a construção de um acervo de materiais didáticos para o ensino de língua inglesa composto, especialmente, por jogos e atividades interativas voltados ao ensino médio integrado. Busca-se produzir materiais de qualidade que possam ter uma vida útil longa e possam ser utilizados por vários professores em diversas turmas. Entendemos que esse acervo irá contribuir muito para o planejamento e a execução das aulas, possibilitando uma metodologia mais dinâmica e interativa, a fim de melhorar a motivação e o engajamento dos estudantes nas aulas de língua inglesa.
+        </v-card-text>
+
+      </v-card>  
+
+    </v-card>    
+
+    <br><br>
+
+    <v-card>
+        <v-card-title>
+          
+        </v-card-title>  
+        <v-card-title>
+          Nossa Instituição<a href="https://ifrs.edu.br/farroupilha/" style="text-decoration:none;"> :Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul – Campus Farroupilha</a>
+          <v-img src="./../assets/logo_ifrs.png"  max-width="50"  class="ml-5 mb-2"></v-img>
+        </v-card-title> 
+        <v-card class="justify-center ma-2 "> 
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1256997.149389916!2d-51.93217879634783!3d-29.51644934712691!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec7177ce45709fc5!2sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%20do%20Rio%20Grande%20do%20Sul%20-%20Campus%20Farroupilha!5e0!3m2!1spt-BR!2sbr!4v1603128365749!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" aria-hidden="false" tabindex="0" width="100%" height="300"></iframe>
+        </v-card>
+    </v-card>
+
+    <br><br>
+
+    <v-card>
+
+      <v-card-title>
+        Equipe
+      </v-card-title>  
+
+
+    </v-card>  
+
+
+
   </div>
 </template>
 
--- src/views/Jogos.vue
+++ src/views/Jogos.vue
@@ -1,7 +1,346 @@
 <template>
-  <div class="Jogos">
-    <h1>This is a Jogos page</h1>
-    <img alt="Vue logo" src="../assets/Logo.png">
+  <v-content>
 
-  </div>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/JogoDaMemoria.png" 
+              alt="Jogo da Memória - Antônimos"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Jogo da Memória - Antônimos
+            </v-card-title>
+            <v-card-text>
+              Um jogo da memória para reforçar o aprendizado de alguns <b>Antônimos</b>.
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/JogoDaForca.png" 
+              alt="Jogo da Forca - Vocabulários Diversos"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Jogo da Forca - Vocabulários Diversos
+            </v-card-title>
+            <v-card-text>
+              Um jogo da forca com temas diversos, como: Profissões, Lugares, Animais, Comidas, Cores e Objetos <br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/JogodosLinks.png" 
+              alt="Jogo Link de Palavras - Vocabulário Animais"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Jogo Link de Palavras - Vocabulário Animais
+            </v-card-title>
+            <v-card-text>
+              Jogo de juntar palavras diversas com as suas respectivas traduções para incentivar o aprendisado de novos <b>vocabularios</b>.<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/Roleta - Adverbio de frequencia.png" 
+              alt="Roleta - Adverbs of Frequency"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Roleta - Adverbs of Frequency
+            </v-card-title>
+            <v-card-text>
+              Uma roleta para sortear <b>advérbios de frequência</b>, para atividades com os alunos<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="http://vizia.co/videos/2332d8016207c3494387f8/share">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/ViziaMartin.png" 
+              alt="Discurso Martin Luther King - Atividade Audiovisual"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Discurso Martin Luther King - Atividade Audiovisual
+            </v-card-title>
+            <v-card-text>
+              Um atividade audiovisual, com o objetivo de ensinar um pouco da <b>história de Martin Luther King</b> e reforçar o Listening<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="http://vizia.co/videos/2332d8016207c3494387f8/share" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="https://create.kahoot.it/details/simple-present-do-does/7c133dbd-d550-4cff-abec-05f4a5f9c47b">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/DoDoesKahoot.png" 
+              alt="Do & Does - Simple Present - Kahoot"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Do & Does - Simple Present - Kahoot
+            </v-card-title>
+            <v-card-text>
+              Um quiz na plataforma kahoot com o conteúdo <b>Do & Does - Simple Present</b><br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="https://create.kahoot.it/details/simple-present-do-does/7c133dbd-d550-4cff-abec-05f4a5f9c47b" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="https://create.kahoot.it/share/passive-voice-inventions/95c57bbc-c80e-4094-91b0-2270f0d79b67">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/CruzadinhaWWII.png" 
+              alt="Passive Voice - Inventions - Kahoot"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Passive Voice - Inventions - Kahoot
+            </v-card-title>
+            <v-card-text>
+              Um quiz na plataforma kahoot com o conteúdo <b>Passive Voice</b>, com perguntas relacionadas com a invenção de objetos<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="https://create.kahoot.it/share/passive-voice-inventions/95c57bbc-c80e-4094-91b0-2270f0d79b67" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/CruzadinhaSimplePresent.png" 
+              alt="Cruzadinha - Present Simple"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Cruzadinha - Present Simple
+            </v-card-title>
+            <v-card-text>
+              Uma Cruzadinha para treinar o conteúdo <b>Present Simple</b><br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/Desembaralhar will e going to.png" 
+              alt="Desembaralhar Palavras - Futuro (Will e Going to)"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Desembaralhar Palavras - Futuro (Will e Going to)
+            </v-card-title>
+            <v-card-text>
+              Uma atividade de desembaralhar palavras para criar frases gramaticalmente corretas no <b>futuro</b>, usando "will" e "going to".<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/CruzadinhaWWII.png" 
+              alt="Logo Stopots"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Cruzadinha - Segunda Guerra Mundial
+            </v-card-title>
+            <v-card-text>
+              Uma Cruzadinha com o conteúdo sobre a <b>Segunda Guerra Mundial</b>, baseada no conteúdo da linha do tempo da plataforma PADLET <br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+              <v-btn href="https://padlet.com/aariottia/1tvymfg55qkf08rg" color="grey">Acessar PADLET</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/Desembaralhar_PresentPerfect.png" 
+              alt="Desembaralhar Palavras - Present Perfect"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Desembaralhar Palavras - Present Perfect
+            </v-card-title>
+            <v-card-text>
+              Um jogo onde aparecem diversas palavras do <b>Present Perfect</b> embaralhadas e você tem que desembaralhar para que a frase fique gramaticalmente correta.<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+    <br><br>
+    <v-card href="#">
+      <v-container fluid fill-height>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-card>
+              <v-img
+              src="../assets/Jogos/Caça Palavras - Past Participle.png" 
+              alt="Caça-Palavras - Past Participle"
+              contain
+              height="300"
+              ></v-img>
+            </v-card>
+            <v-card-title primary-title>   
+              Caça-Palavras - Past Participle
+            </v-card-title>
+            <v-card-text>
+              Um caça-palavras para você treinar a conjugação dos principais verbos no <b>Past Participle</b>.<br>
+            </v-card-text> 
+            <v-card-actions class="justify-center">
+              <v-btn href="#" color="grey">JOGAR</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-card>
+
+
+
+
+
+
+
+  </v-content>  
 </template>
